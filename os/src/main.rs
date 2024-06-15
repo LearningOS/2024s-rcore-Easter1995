f7568caf6386376d9011a3523572e9adee0ae393
Easter1995 <1073208733@qq.com>
2024-06-15 12:56:21 +0800

    尝试引入embassy

--- os/src/main.rs
+++ os/src/main.rs
@@ -12,15 +12,22 @@
 #![no_std] // 告诉 Rust 编译器不使用 Rust 标准库 std 转而使用核心库 core（core库不需要操作系统的支持）
 #![no_main]
 #![feature(panic_info_message)]
+#![feature(type_alias_impl_trait)]
+#![feature(impl_trait_in_assoc_type)]
 
 use core::arch::global_asm;
 use log::*;
+use embassy_executor::Executor;
+use embassy_executor::Spawner;
+use static_cell::StaticCell;
 
 #[macro_use] // 使用 #[macro_use] 注解将模块中定义的宏导入到当前作用域中
 mod console;
 mod lang_items;
 mod logging;
 mod sbi;
+
+static EXECUTOR: StaticCell<Executor> = StaticCell::new();
 
 global_asm!(include_str!("entry.asm"));
 
@@ -36,6 +43,30 @@ pub fn clear_bss() {
 /// the rust entry-point of os
 #[no_mangle]
 pub fn rust_main() -> ! {
+    let executor = EXECUTOR.init(Executor::new());
+    executor.run(|spawner| {
+        spawner.spawn(kernel_start(spawner)).unwrap();
+    });
+}
+
+#[embassy_executor::task]
+async fn test(){
+    let f1 = async {
+        println!("========= async test f1 ==============");
+    };
+    let f2 = async {
+        println!("========= async test f2 ==============");
+    };
+    let f3 = async {
+        println!("========= async test f3 ==============");
+    };
+    f3.await;
+    f2.await;
+    f1.await;
+}
+
+#[embassy_executor::task]
+async fn kernel_start(spawner: Spawner) {
     extern "C" {
         fn stext(); // begin addr of text segment
         fn etext(); // end addr of text segment
@@ -69,7 +100,7 @@ pub fn rust_main() -> ! {
         boot_stack_top as usize, boot_stack_lower_bound as usize
     );
     error!("[kernel] .bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
-
+    spawner.spawn(test()).unwrap();
     // CI autotest success: sbi::shutdown(false)
     // CI autotest failed : sbi::shutdown(true)
     sbi::shutdown(false)
